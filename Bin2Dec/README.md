1f8d5fd6c1d6edaece4b773e8df5ec9a1186fc7e
apsampaio <andre03.sampaio@gmail.com>
2020-02-25 12:17:02 -0300

    finished

--- Bin2Dec/README.md
+++ Bin2Dec/README.md
@@ -22,4 +22,4 @@ and 1's, in any sequence and then displays its decimal equivalent.
 
 ## Result
 
-> See the result at [codepen.io](https://codepen.io/apsampaio/full/dyoOxKZ) [![codepen.io](https://github.com/apsampaio/App-Ideas-Challenge/blob/master/Bin2Dec/img/prev.png?raw=true)](https://codepen.io/apsampaio/full/dyoOxKZ)
+### See the result at [codepen.io](https://codepen.io/apsampaio/full/dyoOxKZ) [![codepen.io](https://github.com/apsampaio/App-Ideas-Challenge/blob/master/Bin2Dec/img/prev.png?raw=true)](https://codepen.io/apsampaio/full/dyoOxKZ)
--- Border Radius Previewer/js/index.js
+++ Border Radius Previewer/js/index.js
@@ -1,3 +1,6 @@
+let complexActive = false;
+$(".complex-holder").hide();
+
 $(".all-borders").on("input change", () => {
   const range = $(".all-borders").val();
   $(".shape").css("border-radius", range + "%");
@@ -10,15 +13,48 @@ $(".bord").on("input change", () => {
   const topRight = $(".top-right").val();
   const bottomLeft = $(".bottom-left").val();
   const bottomRight = $(".bottom-right").val();
+
   $(".tl-value").text(topLeft + "%");
   $(".tr-value").text(topRight + "%");
   $(".bl-value").text(bottomLeft + "%");
   $(".br-value").text(bottomRight + "%");
-  $(".shape").css(
-    "border-radius",
-    `${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}%`
-  );
-  $(".code-output").text(
-    `border-radius: ${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}%;`
-  );
+  if (complexActive) {
+    const cTopLeft = $(".c-top-left").val();
+    const cTopRight = $(".c-top-right").val();
+    const cBottomLeft = $(".c-bottom-left").val();
+    const cBottomRight = $(".c-bottom-right").val();
+
+    $(".c-tl-value").text(cTopLeft + "%");
+    $(".c-tr-value").text(cTopRight + "%");
+    $(".c-bl-value").text(cBottomLeft + "%");
+    $(".c-br-value").text(cBottomRight + "%");
+
+    $(".shape").css(
+      "border-radius",
+      `${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}% / ${cTopLeft}% ${cTopRight}% ${cBottomRight}% ${cBottomLeft}%`
+    );
+
+    $(".code-output").text(
+      `border-radius: ${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}% / ${cTopLeft}% ${cTopRight}% ${cBottomRight}% ${cBottomLeft}%;`
+    );
+  } else {
+    $(".shape").css(
+      "border-radius",
+      `${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}%`
+    );
+
+    $(".code-output").text(
+      `border-radius: ${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}% `
+    );
+  }
+});
+
+$(".copy").on("click", () => {
+  $("#output").select();
+  document.execCommand("copy");
+});
+
+$(".complex").on("click", () => {
+  $(".complex-holder").toggle();
+  complexActive = !complexActive;
 });
